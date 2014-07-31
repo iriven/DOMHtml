af2764875322e5f3630acf578a1cec826693d157
Alfred Tchondjo <iriven@yahoo.fr>
2014-07-31 11:48:24 +0200

    Update DOMHtml.php

--- DOMHtml.php
+++ DOMHtml.php
@@ -1,15 +1,21 @@
 /**
 * DOMHtml - PHP class to manipulate html files, using only the power of DOMDocument and DOMXpath.
-* @Version 1.0.0
-* @package DOMHtml
-* @link https://github.com/iriven/DOMHtml The DOMHtml GitHub project
-* @author Alfred Tchondjo (original founder) <iriven@yahoo.fr>
-* Copyright (C) 2014 Iriven France  
-* ---------------------------------------------------------------
+* Copyright (C) 2014 Iriven France Software, Inc. 
 *
+* Licensed under The GPL V3 License
+* Redistributions of files must retain the above copyright notice.
+*
+* @Copyright 		Copyright (C) 2014 Iriven France Software, Inc.
+* @package 		DOMHtml
+* @Since 		Version 1.0.0
+* @link 		https://github.com/iriven/DOMHtml The DOMHtml GitHub project
+* @author 		Alfred Tchondjo (original founder) <iriven@yahoo.fr>
+* @license  		GPL V3 License(http://www.opensource.org/licenses/mit-license.php)
+*
+* ==================  NOTICE  =======================
 * This program is free software; you can redistribute it and/or
 * modify it under the terms of the GNU General Public License
-* as published by the Free Software Foundation; either version 2
+* as published by the Free Software Foundation; either version 3
 * of the License, or any later version.
 *
 * This program is distributed in the hope that it will be useful,
